cf2182f7035e94e3c3e788e848a8bb6b61bb4b6a
Brian De Sousa <briandesousa1@gmail.com>
2020-02-28 01:46:59 -0500

    added persistence using firestore

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 The Live Grocery List app is a demonstration of using React Hooks to manage app state that is backed by a Firebase firestore backend. This app demonstrates using building in React hooks as well as custom hooks.
 
+## Setup
+
+This application uses Firebase services. Configuration required to connect to Firebase is defined in the [.env](.env) file (or .env.local) in the root of this repository.
+
+Before building or running the app, you must add your Firebase project configuration. Configuration can be obtained from *Project Settings* in your Firebase project at [https://console.firebase.google.com](https://console.firebase.google.com).
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/services/firestore.js
+++ src/services/firestore.js
@@ -1,54 +1,66 @@
-// create a list on Firestore and return listId
-export const createList = (userName) => {
-    return new Promise( resolve => {
-        // TODO create on firestore
-        resolve({
-            listId: 1,
-            users: [{ name: userName }],
-            items: []
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+export const createGroceryList = (userName) => {
+    return db.collection('groceryLists')
+        .add({
+            created: firebase.firestore.FieldValue.serverTimestamp(),
+            users: [{ name: userName}]
         });
-    });
 };
 
-// update a list with a new user
-export const addUserToList = (userName, list) => {
-    return new Promise( resolve => {
-        // TODO update on firestore
-        let updatedList = { ...list };
-        updatedList.users.push({ name: userName});
-        resolve(updatedList);
-    });
-    
+export const getGroceryList = groceryListId => {
+    return db.collection('groceryLists')
+        .doc(groceryListId)
+        .get();
 };
 
-// update a list with a new item
-export const addItemToList = (item, list) => {
-    console.log(item);
-    return new Promise( resolve => {
-        // TODO update on firestore
-        let updatedList = { ...list };
-        updatedList.items.push({ name: item });
-        console.log(updatedList);
-        resolve(updatedList);
-    }); 
+export const getGroceryListItems = groceryListId => {
+    return db.collection('groceryLists')
+        .doc(groceryListId)
+        .collection('items')
+        .get();
 }
 
-// retrieve list by id
-export const getListById = listId => {
-    return new Promise((resolve, reject) => {
-        // TODO query firestore
-        if (parseInt(listId) === 1) {
-            resolve({
-                listId: '1',
-                users: [
-                    { name: 'Brian' },
-                    { name: 'Holly' },
-                    { name: 'Steve' }
-                ],
-                items: []
-            });
-        } else {
-            reject('invalid list id');
-        }
-    });
-}
+export const streamGroceryListItems = (groceryListId, observer) => {
+    return db.collection('groceryLists')
+        .doc(groceryListId)
+        .collection('items')
+        .orderBy('created')
+        .onSnapshot(observer);
+};
+
+export const addUserToGroceryList = (userName, groceryListId) => {
+    return db.collection('groceryLists')
+        .doc(groceryListId)
+        .update({
+            users: firebase.firestore.FieldValue.arrayUnion({ name: userName})
+        });
+};
+
+export const addGroceryListItem = (item, groceryListId) => {
+    return getGroceryListItems(groceryListId)
+        .then(querySnapshot => querySnapshot.docs)
+        .then(groceryListItems => groceryListItems.find(groceryListItem => groceryListItem.data().name.toLowerCase() === item.toLowerCase()))
+        .then(matchingItem => {
+            if (!matchingItem) {
+                return db.collection('groceryLists')
+                    .doc(groceryListId)
+                    .collection('items')
+                    .add({
+                        name: item,
+                        created: firebase.firestore.FieldValue.serverTimestamp()
+                    });
+            }
+            throw new Error('duplicate-item-error');
+        });
+};
