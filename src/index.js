d7bebe7ab780539b1def5e60fa5a41a0a0b22197
findmory <findmory@gmail.com>
2020-06-11 09:52:19 -0400

    update react-scripts to 3.4.1 to have react 16.12 and recoil support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>,
+  document.getElementById("root")
+);
